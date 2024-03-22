208e3983f2fe020e94081bfeae4a5f4ee3a97e7f
akkiadepu <akki.adepu@gmail.com>
2024-03-22 12:14:06 +0530

    restcontorller update

--- DoctorManagementSystem/src/main/java/com/codegnan/controller/HomeController.java
+++ DoctorManagementSystem/src/main/java/com/codegnan/controller/HomeController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 @CrossOrigin(origins = "0.0.0.0")
 public class HomeController {
