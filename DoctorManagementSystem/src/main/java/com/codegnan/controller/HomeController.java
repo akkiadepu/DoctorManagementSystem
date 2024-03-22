626e523abec8543432231eebc6ed8aa24a41fb18
akkiadepu <akki.adepu@gmail.com>
2024-03-22 11:57:10 +0530

    home controller

--- DoctorManagementSystem/src/main/java/com/codegnan/controller/HomeController.java
+++ DoctorManagementSystem/src/main/java/com/codegnan/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.codegnan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@CrossOrigin(origins = "0.0.0.0")
 public class HomeController {
-	@GetMapping
+	@GetMapping("/")
 	public String home() {
 		return "Welcome to DoctorManagementSystem REST Application";
 	}
